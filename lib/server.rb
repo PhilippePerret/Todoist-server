e7829ba9395948bf9b3d89fe38af69bc7564f08d
PhilippePerret <philippe.perret@yahoo.fr>
2026-05-21 07:34:41 +0200

    Lancement automatique du minuteur

--- lib/server.rb
+++ lib/server.rb
@@ -1,27 +1,37 @@
 class Server
 
   def self.force_init
-
+    
+    
 
     
-fork do
-  Process.daemon
+script = <<~BASH
+sleep 2
 
-  system(
-    "pkill -f 'com.apple.Safari.WebApp.D6445759-72F9-4B08-B0C5-10E5F1DE660C'"
-  )
+pkill -f 'com.apple.Safari.WebApp.D6445759-72F9-4B08-B0C5-10E5F1DE660C'
 
-  sleep 1
+while pgrep -f 'com.apple.Safari.WebApp.D6445759-72F9-4B08-B0C5-10E5F1DE660C' >/dev/null
+do
+  sleep 0.2
+done
 
-  system(
-    "rm -f ~/Library/Containers/com.apple.Safari.WebApp/Data/Library/Containers/com.apple.Safari.WebApp.D6445759-72F9-4B08-B0C5-10E5F1DE660C/Library/WebApp/DocumentsState.plist"
-  )
+rm -f ~/Library/Containers/com.apple.Safari.WebApp/Data/Library/Containers/com.apple.Safari.WebApp.D6445759-72F9-4B08-B0C5-10E5F1DE660C/Library/WebApp/DocumentsState.plist
 
-  system("open /Applications/TodoistServer.app")
-end
+sleep 1
+
+open /Applications/TodoistServer.app
+BASH
+
+Process.spawn(
+  "/usr/bin/nohup",
+  "/bin/bash",
+  "-c",
+  script,
+  out: "/tmp/webapp-reset.log",
+  err: "/tmp/webapp-reset.log"
+)
 
 exit
-
 
 
 
